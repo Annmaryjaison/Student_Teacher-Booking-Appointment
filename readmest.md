a1bfb3b3683a5ec3c6ec901f68f79ef53d4e5391
Annmaryjaison <annmaryjaison05@gmail.com>
2025-09-14 13:05:57 +0530

    Add files via upload

---
+++ readmest.md
@@ -0,0 +1,35 @@
+1.download the file.
+
+2.extract the file and paste it inside the root directory.
+
+3.Open PHPMyAdmin
+(http://localhost/phpmyadmin)
+
+4.Create a database with name student_teacher
+
+5.import student_teacher_booking.sql file(give in the file folder )
+
+6.Run the script http://localhost/catering
+
+Admin credentials
+username:admin
+email:admin@gmil.com
+password:password
+
+teachers credentials
+
+username: Boban Philip
+email:bobanphilip@gmail.com
+password:boban
+
+student credentials
+
+username: Ann Jaison
+email: annjaison@gmail.com
+password: annjaison
+
+username: Maris Sajan
+email: mariasajan@gmail.com
+password: maria 
+
+
---
+++ student_teacher_booking.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2025 at 11:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_teacher_booking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `username`, `password_hash`) VALUES
+(1, 'admin', '$2y$10$GlM8hvsryjTz6ULIvNauY.Aqvi4AsoJ7NWfryu4/Zdl4Ij5rlpU5.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `appointment_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  `availability_id` int(11) DEFAULT NULL,
+  `status` enum('pending','confirmed','canceled') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`appointment_id`, `student_id`, `teacher_id`, `availability_id`, `status`) VALUES
+(1, 1, 101, 11, 'pending'),
+(2, 2, 102, 12, 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `message_text` text NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  `sender_type` enum('student','teacher') NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `sender_id`, `recipient_id`, `message_text`, `timestamp`, `sender_type`, `is_read`, `sent_at`) VALUES
+(1, 1, 102, 'hi sir', '2025-09-02 17:36:26', 'student', 1, '2025-09-02 12:06:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `name`, `email`, `password_hash`, `status`) VALUES
+(1, 'Ann Jaison', 'annjaison@gmail.com', '$2y$10$eSjviTLYbhoQazGN6SPstuwzi.x1FjSt9KbINhOzeR3MtsqcoGkja', 'approved'),
+(2, 'Maria Sajan', 'mariasajan@gmail.com', '$2y$10$FM0UGjqHWjE29T9aV4f.fOh1bVQGvcPUAyCfJDrqdgUiSicXIwNNq', 'approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `subject` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`teacher_id`, `name`, `email`, `password_hash`, `subject`) VALUES
+(101, 'Anitha Nair', 'anithanair@gmail.com', 'annitha', 'English'),
+(102, 'Boban Philip', 'bobanphilip@gmail.com', '$2y$10$zb39zmtPaeGF37IStA1qZ.g.4qpXC.Ehxmp5FTXbWFir6Aqe2CSXy', 'Chemistry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_availability`
+--
+
+CREATE TABLE `teacher_availability` (
+  `availability_id` int(11) NOT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `is_booked` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher_availability`
+--
+
+INSERT INTO `teacher_availability` (`availability_id`, `teacher_id`, `start_time`, `end_time`, `is_booked`) VALUES
+(11, 101, '2025-08-23 15:11:14', '2025-08-31 15:11:14', 1),
+(12, 102, '2025-08-30 09:30:00', '2025-08-30 10:30:00', 1),
+(13, 102, '2025-09-18 10:45:00', '2025-09-18 11:45:00', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`appointment_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `availability_id` (`availability_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `teacher_availability`
+--
+ALTER TABLE `teacher_availability`
+  ADD PRIMARY KEY (`availability_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+
+--
+-- AUTO_INCREMENT for table `teacher_availability`
+--
+ALTER TABLE `teacher_availability`
+  MODIFY `availability_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  ADD CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`teacher_id`),
+  ADD CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`availability_id`) REFERENCES `teacher_availability` (`availability_id`);
+
+--
+-- Constraints for table `teacher_availability`
+--
+ALTER TABLE `teacher_availability`
+  ADD CONSTRAINT `teacher_availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`teacher_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
